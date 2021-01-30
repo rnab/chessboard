ca8fa614d7b971402224f27883830df7db8a7882
Ricki N <riknabeshima@gmail.com>
2021-01-30 23:53:19 +0000

    Generalised CBTest to full board and tested w 3x3

--- CBTest.py
+++ CBTest.py
@@ -2,51 +2,33 @@ import RPi.GPIO as GPIO
 from time import sleep
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
-r1 = 14
-r2 = 15
-r3 = 18
 
-c1 = 16
-c2 = 20
-c3 = 21
+cols={2:'f',3:'g',4:'h'}
+rows={14:'1',15:'2',18:'3'}
 
-GPIO.setup(r1,GPIO.OUT)
-GPIO.setup(r2,GPIO.OUT)
-GPIO.setup(r3,GPIO.OUT)
-
-GPIO.output(r1,GPIO.LOW)
-GPIO.output(r2,GPIO.LOW)
-GPIO.output(r3,GPIO.LOW)
-
-GPIO.setup(c1,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(c2,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(c3,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
-
-
+#Set up GPIO
 
 def get_board_input():
+	for c in cols.keys():
+		GPIO.setup(c,GPIO.OUT)
+		GPIO.output(c,GPIO.LOW)
+
+	for r in rows.keys():
+		GPIO.setup(r,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
+	
 	while True:
-		GPIO.output(r1,GPIO.HIGH)
-		if GPIO.input(c1)==1:
-			GPIO.output(r1,GPIO.LOW)
-			return("button1")
-		GPIO.output(r1,GPIO.LOW)
-		GPIO.output(r2,GPIO.HIGH)
-		
-		if GPIO.input(c2) == 1:
-			GPIO.output(r2,GPIO.LOW)
-			return("button2")
-		GPIO.output(r2,GPIO.LOW)
-		GPIO.output(r3,GPIO.HIGH)
-		
-		if GPIO.input(c3) == 1:
-			GPIO.output(r3,GPIO.LOW)
-			return("button3")
-		GPIO.output(r3,GPIO.LOW)
+		for c in cols.keys():
+			GPIO.output(c,GPIO.HIGH)
+			for r in rows.keys():
+				if GPIO.input(r)==1:
+					return(cols[c]+rows[r])
+				sleep(0.01)
+			GPIO.output(c,GPIO.LOW)
+
 		
 def get_board_stream():
 	while True:
 		print(get_board_input())
-		sleep(0.5)
+		sleep(0.1)
 
-#get_board_input()
+get_board_stream()
