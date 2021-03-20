cefb1d4c8eb7f6f160f386ea20fde32aacf02db7
Ricki <riknabeshima@gmail.com>
2021-03-20 22:55:11 +0000

    Test Reed / LED working

--- LEDShiftFunctions.py
+++ LEDShiftFunctions.py
@@ -5,17 +5,13 @@ GPIO.setmode(GPIO.BCM)
 
 c_data=3
 a_data=2
-c_clock=17
-a_clock=17
-c_shift=4
-a_shift=4
+clock=17
+shift=4
 
 GPIO.setup(a_data,GPIO.OUT)
 GPIO.setup(c_data,GPIO.OUT)
-GPIO.setup(a_clock,GPIO.OUT)
-GPIO.setup(c_clock,GPIO.OUT)
-GPIO.setup(a_shift,GPIO.OUT)
-GPIO.setup(c_shift,GPIO.OUT)
+GPIO.setup(clock,GPIO.OUT)
+GPIO.setup(shift,GPIO.OUT)
 
 def led_on(c,r):
 	#Set Cathode all high except target
@@ -29,39 +25,39 @@ def led_on(c,r):
 		else:
 			GPIO.output(a_data,GPIO.LOW)
 		sleep(0.001)
-		GPIO.output(c_clock,GPIO.HIGH)            # pull CLOCK pin high
+		GPIO.output(clock,GPIO.HIGH)            # pull CLOCK pin high
 		sleep(0.001)
-		GPIO.output(c_clock,GPIO.LOW)            # pull CLOCK pin down, to send a rising edge
+		GPIO.output(clock,GPIO.LOW)            # pull CLOCK pin down, to send a rising edge
 		GPIO.output(c_data,GPIO.LOW)       # clear the DATA pin
-	GPIO.output(c_shift,GPIO.HIGH)      # pull the SHIFT pin high to put the 8 bit data out parallel
+	GPIO.output(shift,GPIO.HIGH)      # pull the SHIFT pin high to put the 8 bit data out parallel
 	sleep(0.001)
-	GPIO.output(c_shift,GPIO.LOW) 
+	GPIO.output(shift,GPIO.LOW) 
 
 
 def led_off():
 	for y in range(8):
 		GPIO.output(a_data,GPIO.LOW)
 		sleep(0.001)
-		GPIO.output(a_clock,GPIO.HIGH)
+		GPIO.output(clock,GPIO.HIGH)
 		sleep(0.001)
-		GPIO.output(a_clock,GPIO.LOW)
+		GPIO.output(clock,GPIO.LOW)
 		GPIO.output(a_data,GPIO.LOW)
-	GPIO.output(a_shift,GPIO.HIGH)
-	GPIO.output(a_shift,GPIO.LOW)
+	GPIO.output(shift,GPIO.HIGH)
+	GPIO.output(shift,GPIO.LOW)
 	for y in range(8):
 		GPIO.output(c_data,GPIO.HIGH)
 		sleep(0.001)
-		GPIO.output(c_clock,GPIO.HIGH)            # pull CLOCK pin high
+		GPIO.output(clock,GPIO.HIGH)            # pull CLOCK pin high
 		sleep(0.001)
-		GPIO.output(c_clock,GPIO.LOW)            # pull CLOCK pin down, to send a rising edge
+		GPIO.output(clock,GPIO.LOW)            # pull CLOCK pin down, to send a rising edge
 		GPIO.output(c_data,GPIO.LOW)       # clear the DATA pin
-	GPIO.output(c_shift,GPIO.HIGH)      # pull the SHIFT pin high to put the 8 bit data out parallel
-	GPIO.output(c_shift,GPIO.LOW)  
+	GPIO.output(shift,GPIO.HIGH)      # pull the SHIFT pin high to put the 8 bit data out parallel
+	GPIO.output(shift,GPIO.LOW)  
 
-led_off()
-led_on([0,1,2,3,4,5,6,7],[0,1,2,3,4,5,6,7])
-sleep(5)
-led_off()
-led_on([0],[0])
-sleep(1)
-led_off()
+#led_off()
+#led_on([0,2],[0,2])
+#sleep(5)
+#led_off()
+#led_on([0],[0])
+#sleep(1)
+#led_off()
--- ReedInput.py
+++ ReedInput.py
@@ -2,6 +2,7 @@ import RPi.GPIO as GPIO
 from time import sleep
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
+import LEDShiftFunctions as led
 
 
 cols={27:'h',22:'g',10:'f',9:'e',11:'d',5:'c',6:'b',13:'a'}
@@ -28,4 +29,37 @@ def get_board_stream():
 		print(get_board_input())
 		sleep(0.1)
 
-get_board_stream()
+def read_board():
+	board=[0]*64
+	i=0
+	for c in cols.keys():
+		GPIO.setup(c,GPIO.OUT)
+		GPIO.output(c,GPIO.LOW)
+	for r in rows.keys():
+		GPIO.setup(r,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
+	
+	for c in cols.keys():
+		GPIO.output(c,GPIO.HIGH)
+		for r in rows.keys():
+			if GPIO.input(r)==1:
+				board[i]=1
+			else:
+				board[i]=0
+			i=i+1
+			sleep(0.0001)
+		GPIO.output(c,GPIO.LOW)
+	return(board)
+
+def get_rc(board):
+	led.led_off()
+	z=0
+	for i in board:
+		if i == 1:
+			led.led_on([int(z/8)],[z%8])
+			sleep(0.1)
+		z=z+1
+
+
+while True:
+	board=read_board()
+	get_rc(board)
